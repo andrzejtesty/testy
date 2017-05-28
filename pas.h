6764dd995ad20c9f423d8a764dd56e283d4b20ca
andrzejtesty <tadeusz.marusiak@gmail.com>
2017-05-28 10:23:14 +0200

    Add files via upload

--- pas.h
+++ pas.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
+class Pas
+{
+	int ID_samolotu;
+	int ID_pasa;
+public:
+	Pas(int a);
+	bool czyWolny();
+	int podajIdentyfikator();
+	int podajSamolot();
+	std::string podajStan();
+	void przypiszSamolot(int);
+	void zwolnij();
+};
 
